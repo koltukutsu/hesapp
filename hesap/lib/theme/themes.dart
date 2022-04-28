53f5c73665d95f02fa0d2eed4be7a23623dde471
Erdem Kalyoncu <ekalyoncu81@gmail.com>
2022-04-28 18:46:00 +0300

    Temel klasörler oluşturuldu

---
+++ hesap/lib/theme/themes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:hesap/theme/colors.dart';
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: AppColors.primary,
+);
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: AppColors.primary,
+);
