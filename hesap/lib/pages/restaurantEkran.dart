8ce05b741d740bf21455a07a8525e144b170d8c7
M.Zeynep Yavuz <melikezeynep_97@hotmail.com>
2022-05-06 14:32:58 +0300

    SliverAppBar kullanıma hazır.

--- hesap/lib/pages/restaurantEkran.dart
+++ hesap/lib/pages/restaurantEkran.dart
@@ -27,7 +27,7 @@ class _RestaurantEkran extends State<RestaurantEkran> {
               delegate: SliverAppBar(
                 maxYukseklik: 300.0,
               ),
-              pinned: false,
+              pinned: true,
             ),
             SliverList(
                 delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
@@ -52,7 +52,8 @@ class SliverAppBar extends SliverPersistentHeaderDelegate {
   Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
     var adjustedShrinkOffset =
     shrinkOffset > minExtent ? minExtent : shrinkOffset;
-    double offset = (minExtent - adjustedShrinkOffset) * 0.5;
+    double offset = (minExtent - adjustedShrinkOffset) -20;
+    double topPadding = MediaQuery.of(context).padding.top + 250;
 
     return Stack(
       alignment: AlignmentDirectional.topEnd,
@@ -69,13 +70,14 @@ class SliverAppBar extends SliverPersistentHeaderDelegate {
             ),
             child: Container(
               width: MediaQuery.of(context).size.width,
-              height:210,
+              height: topPadding-55,
               color: AppColors.primary,
             ),
           ),
         ),
-        SvgPicture.asset('assets/images/background.svg'),
+        //SvgPicture.asset('assets/images/background.svg'),
         SizedBox(
+          height: topPadding -20,
           width: MediaQuery.of(context).size.width,
           child: const Padding(
             padding: EdgeInsets.all(18.0),
@@ -113,14 +115,14 @@ class SliverAppBar extends SliverPersistentHeaderDelegate {
           ),
         ),
         SizedBox(
-          height: 250,
+          height: 120 + offset,
           child: Align(
             alignment: AlignmentDirectional.bottomCenter,
             child: ClipRRect(
               borderRadius: const BorderRadius.all(Radius.circular(10)),
               child: Container(
-                width: MediaQuery.of(context).size.width*0.82,
-                height: 80,
+                width: MediaQuery.of(context).size.width * 0.82,
+                height: 90,
                 color: Colors.white,
                 child: Row(
                   mainAxisSize: MainAxisSize.max,
@@ -155,30 +157,33 @@ class SliverAppBar extends SliverPersistentHeaderDelegate {
           ),
         ),
         Positioned(
-          top: maxYukseklik - 70 + offset,
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text(
-                    'Yakındaki Mekanlar',
-                    style: TextStyle(
-                      fontSize: 20,
+          top: 110 + offset ,
+          child: Align(
+            alignment: AlignmentDirectional.bottomCenter,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width -20,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(3.0),
+                    child: Text(
+                      'Yakındaki Mekanlar',
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Icon(
-                    Icons.search,
-                    size: 40,
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Icon(
+                      Icons.search,
+                      size: 40,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -187,10 +192,10 @@ class SliverAppBar extends SliverPersistentHeaderDelegate {
   }
 
   @override
-  double get maxExtent => maxYukseklik ;
+  double get maxExtent => maxYukseklik  ;
 
   @override
-  double get minExtent => 25;
+  double get minExtent => 140;
 
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
