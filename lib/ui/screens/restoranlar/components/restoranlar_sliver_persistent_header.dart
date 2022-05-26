f7c1d316dc6d6b36f0ad75e1a7f327e039ca35af
M.Zeynep <95376085+mzyavuz@users.noreply.github.com>
2022-05-26 15:25:38 +0300

    (Semih için) Stack widgetlara bölündü

--- lib/ui/screens/restoranlar/components/restoranlar_sliver_persistent_header.dart
+++ lib/ui/screens/restoranlar/components/restoranlar_sliver_persistent_header.dart
@@ -27,18 +27,14 @@ class _SliverHeaderState extends State<SliverHeader> {
   @override
   Widget build(BuildContext context) {
     return SliverPersistentHeader(
-      delegate: SliverAppBar(
-        maxYukseklik: 300.0,
-      ),
+      delegate: SliverAppBar(),
       pinned: true,
     );
   }
 }
 
 class SliverAppBar extends SliverPersistentHeaderDelegate {
-  final double maxYukseklik;
-
-  SliverAppBar({required this.maxYukseklik});
+  static const double maxYukseklik = 300;
 
   @override
   Widget build(
@@ -48,10 +44,11 @@ class SliverAppBar extends SliverPersistentHeaderDelegate {
     double offset = (minExtent - adjustedShrinkOffset) - 20;
     double topPadding = MediaQuery.of(context).padding.top + 250;
 
+
     return Stack(
       alignment: AlignmentDirectional.topEnd,
       children: [
-        SizedBox(
+        const SizedBox(
           height: maxYukseklik,
         ),
         Transform.scale(
@@ -69,46 +66,8 @@ class SliverAppBar extends SliverPersistentHeaderDelegate {
           ),
         ),
         SvgPicture.asset('assets/images/background.svg'),
-        Align(
-          alignment: AlignmentDirectional.topStart,
-          child: SizedBox(
-            height: topPadding - 180,
-            width: (150 - offset) * 0.7,
-            //TODO: Ya da visible olacak
-            child: IconButton(
-              onPressed: () {
-                //TODO: ROUTE GELECEK
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const GirisYapEkran(),
-                  ),
-                );
-              },
-              icon: const Icon(
-                Icons.person,
-                size: 40,
-                color: AppColors.white,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: topPadding - 20,
-          width: MediaQuery.of(context).size.width,
-          child: const Padding(
-            padding: EdgeInsets.all(18.0),
-            child: Text(
-              'hesap',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Ubuntu',
-                color: AppColors.white,
-                fontSize: 25,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ),
-        ),
+        UserIkonu(topPadding: topPadding, offset: offset),
+        HesapYazisi(topPadding: topPadding),
         SizedBox(
           width: MediaQuery.of(context).size.width,
           height: 210,
@@ -124,7 +83,7 @@ class SliverAppBar extends SliverPersistentHeaderDelegate {
                 builder: (context, state) {
                   if (state is KonumYuklendi) {
                     return Text(
-                      '${state.adres.first.street.toString()} , ${state.adres.first.administrativeArea}',
+                      '${state.adres.first.street.toString()} ${state.adres.first.subLocality}, ${state.adres.first.locality}',
                       textAlign: TextAlign.center,
                       style: const TextStyle(
                           color: AppColors.white,
@@ -139,115 +98,8 @@ class SliverAppBar extends SliverPersistentHeaderDelegate {
             ],
           ),
         ),
-        SizedBox(
-          height: 120 + offset,
-          child: Align(
-            alignment: AlignmentDirectional.bottomCenter,
-            child: ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.82,
-                height: 90,
-                margin: const EdgeInsets.only(bottom: 6.0),
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: AppColors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: AppColors.gray,
-                        offset: Offset(0.0, 2.5),
-                        blurRadius: 6.0,
-                      )
-                    ]),
-                child: InkWell(
-                  onTap: () {
-                    //TODO: ROUTE olacak
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const QrOkumaEkran(),
-                      ),
-                    );
-                  },
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child: Container(
-                          margin: const EdgeInsets.all(18),
-                          alignment: Alignment.center,
-                          child: const Text(
-                            'Menüyü görüntülemek için QR kodu okutun',
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: AppColors.darkBackground,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.qr_code,
-                          size: 60,
-                          color: AppColors.darkBackground,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          top: 110 + offset,
-          right: 10,
-          child: Align(
-            alignment: AlignmentDirectional.bottomCenter,
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width - 30,
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(3.0),
-                    child: Text(
-                      'Yakındaki Mekanlar',
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontFamily: ' Ubuntu',
-                        color: AppColors.darkBackground,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.search,
-                        size: 40,
-                        color: AppColors.darkBackground,
-                      ),
-                      onPressed: () {
-                        showSearch(
-                          context: context,
-                          delegate: RestoranAramaTemsilcisi(
-                            hintText: 'Restoran Ara',
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+        QRKodOkutma(offset: offset),
+        YakinimdakiMekanlar(offset: offset),
       ],
     );
   }
@@ -261,4 +113,207 @@ class SliverAppBar extends SliverPersistentHeaderDelegate {
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
       oldDelegate.maxExtent != maxExtent || oldDelegate.minExtent != minExtent;
+}
+
+class UserIkonu extends StatelessWidget {
+  const UserIkonu({
+    Key? key,
+    required this.topPadding,
+    required this.offset,
+  }) : super(key: key);
+
+  final double topPadding;
+  final double offset;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: AlignmentDirectional.topStart,
+      child: SizedBox(
+        height: topPadding - 180,
+        width: (150 - offset) * 0.7,
+        //TODO: Ya da visible olacak
+        child: IconButton(
+          onPressed: () {
+            //TODO: ROUTE GELECEK
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const GirisYapEkran(),
+              ),
+            );
+          },
+          icon: const Icon(
+            Icons.person,
+            size: 40,
+            color: AppColors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HesapYazisi extends StatelessWidget {
+  const HesapYazisi({
+    Key? key,
+    required this.topPadding,
+  }) : super(key: key);
+
+  final double topPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: topPadding - 20,
+      width: MediaQuery.of(context).size.width,
+      child: const Padding(
+        padding: EdgeInsets.all(18.0),
+        child: Text(
+          'hesap',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontFamily: 'Ubuntu',
+            color: AppColors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QRKodOkutma extends StatelessWidget {
+  const QRKodOkutma({
+    Key? key,
+    required this.offset,
+  }) : super(key: key);
+
+  final double offset;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120 + offset,
+      child: Align(
+        alignment: AlignmentDirectional.bottomCenter,
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.82,
+            height: 90,
+            margin: const EdgeInsets.only(bottom: 6.0),
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: AppColors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.gray,
+                    offset: Offset(0.0, 2.5),
+                    blurRadius: 6.0,
+                  )
+                ]),
+            child: InkWell(
+              onTap: () {
+                //TODO: ROUTE olacak
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const QrOkumaEkran(),
+                  ),
+                );
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Container(
+                      margin: const EdgeInsets.all(18),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Menüyü görüntülemek için QR kodu okutun',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: AppColors.darkBackground,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.qr_code,
+                      size: 60,
+                      color: AppColors.darkBackground,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class YakinimdakiMekanlar extends StatelessWidget {
+  const YakinimdakiMekanlar({
+    Key? key,
+    required this.offset,
+  }) : super(key: key);
+
+  final double offset;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 110 + offset,
+      right: 10,
+      child: Align(
+        alignment: AlignmentDirectional.bottomCenter,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width - 30,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(3.0),
+                child: Text(
+                  'Yakındaki Mekanlar',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontFamily: ' Ubuntu',
+                    color: AppColors.darkBackground,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.search,
+                    size: 40,
+                    color: AppColors.darkBackground,
+                  ),
+                  onPressed: () {
+                    showSearch(
+                      context: context,
+                      delegate: RestoranAramaTemsilcisi(
+                        hintText: 'Restoran Ara',
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
