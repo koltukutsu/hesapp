6cd54f97c73f641a7964c96b7d9f52e5ae92dc26
M.Zeynep <95376085+mzyavuz@users.noreply.github.com>
2022-06-06 14:13:26 +0300

    Menu sliver oldu

--- lib/ui/screens/menu/menu_ekran.dart
+++ lib/ui/screens/menu/menu_ekran.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 // components
 import 'package:hesap/ui/screens/menu/components/menu_body.dart';
+import 'package:hesap/ui/screens/menu/components/menu_header.dart';
 
 class MenuEkran extends StatefulWidget {
   const MenuEkran({Key? key}) : super(key: key);
@@ -16,15 +17,22 @@ class _MenuEkranState extends State<MenuEkran> {
   @override
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         // appBar: AppBar(
         //   title: const Text("Menü"),
         //   backgroundColor: AppColors.primary,
         // ),
-        body: SingleChildScrollView(
-          child: MenuBody(
-      restaurantId: "TyZa1uLFz27YKTH7Yhy2",
-    ),
-        ));
+        body: CustomScrollView(
+            slivers: [
+              const SliverMenuHeader(),
+              SliverList(
+                delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+                  return const MenuBody(
+                    restaurantId: "TyZa1uLFz27YKTH7Yhy2",
+                  );
+        },childCount: 1,
+        ),
+      ),
+    ]));
   }
 }
